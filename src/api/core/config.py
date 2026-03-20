1ed1346729f69fb8bf8a944bbcfccd64664abd2c
kselig1 <198694969+kselig1@users.noreply.github.com>
2026-03-20 11:30:11 -0700

    updated debugging config

--- src/api/core/config.py
+++ src/api/core/config.py
@@ -5,6 +5,6 @@ class Config(BaseSettings):
     GROQ_API_KEY: str   
     GOOGLE_API_KEY: str  
 
-    model_config = SettingsConfigDict(env_file = ".env")
+    model_config = SettingsConfigDict(env_file = ".env", extra="ignore")
 
 config = Config()
